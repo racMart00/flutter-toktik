0521219b16e84827f4cfcc4b4d44d122696dddda
racmart00 <racmart00@gmail.com>
2024-03-19 15:04:36 -0400

    second commit

--- README.md
+++ README.md
@@ -14,3 +14,21 @@ A few resources to get you started if this is your first Flutter project:
 For help getting started with Flutter development, view the
 [online documentation](https://docs.flutter.dev/), which offers tutorials,
 samples, guidance on mobile development, and a full API reference.
+
+-->Pasos para hacer primer commit<--
+git init
+
+git add .
+
+git commit -m "first commit"
+
+git remote add origin https://github.com/NOMBRE_USUARIO/NOMBRE_PROYECTO.git
+
+git push -u origin master
+
+-->Flujo de la Arquitectura Limpia<--
+1. UI: Tiene la comunicacion con la capa de presentacion
+2. Presentacion: Tiene providers o gestionadores de estado q terminan llamando los casos de uso
+3. Casos de Uso: Se comunica con los repositorios //NO SE HACE EN ESTE PROYECTO
+4. Repositorios y Datasources: Los repositorios llaman los datasources
+5. Informacion: Y luego la informacion regresa al UI.
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import 'package:toktik/config/theme/app_theme.dart';
+import 'package:toktik/infrastructure/datasource/local_video_datasorce_impl.dart';
+import 'package:toktik/infrastructure/repositories/video_posts_repository_impl.dart';
 import 'package:toktik/presentation/providers/discover_provider.dart';
 import 'package:toktik/presentation/screens/discover/discover_screen.dart';
 
@@ -12,11 +14,15 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final videoPostRepository = VideoPostsRepositoryImpl(
+      videosDatasource: LocalVideoDatasource(),
+    );
+
     return MultiProvider(
       providers: [
         ChangeNotifierProvider(
           lazy: false,
-          create: (_) => DiscoverProvider()..loadNextPage()
+          create: (_) => DiscoverProvider( videosRepository: videoPostRepository )..loadNextPage()
         )
       ],
       child: MaterialApp(
